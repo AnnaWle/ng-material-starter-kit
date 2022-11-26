78ae795184e2d2a8a5f3e7c132c6ef997cac19fb
Anna Wleklinska <awleklinska@gmail.com>
2022-11-26 05:02:19 +0100

    feat: MatCard Layout added

--- src/app/ui/product-table/product-table.component-module.ts
+++ src/app/ui/product-table/product-table.component-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { ProductTableComponent } from './product-table.component';
 
 @NgModule({
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatCardModule],
   declarations: [ProductTableComponent],
   providers: [],
   exports: [ProductTableComponent]
