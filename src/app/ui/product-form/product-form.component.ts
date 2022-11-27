c8fdce663dd8ad595768a9f0170d71380bc06f2b
Anna Wleklinska <awleklinska@gmail.com>
2022-11-27 06:00:39 +0100

    feat: extend product form with categories list implemented

--- src/app/ui/product-form/product-form.component.ts
+++ src/app/ui/product-form/product-form.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { ProductModel } from '../../model/product.model';
 import { ProductsService } from '../../services/products.service';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-product-form',
@@ -16,8 +19,9 @@ export class ProductFormComponent {
     category: new FormControl(),
     image: new FormControl()
   });
+  readonly list$: Observable<ProductModel[]> = this._categoriesService.getAll();
 
-  constructor(private _productsService: ProductsService) {
+  constructor(private _productsService: ProductsService, private _categoriesService: CategoriesService) {
   }
 
   onProductFormSubmitted(productForm: FormGroup): void {
